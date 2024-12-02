419f3bc655773b9ecd27bf459ccfa5db864f5aa7
stefan1336 <stefanjohansson1992@hotmail.com>
2024-12-02 17:48:34 +0100

    created-project

--- src/Java/Vehicle.java
+++ src/Java/Vehicle.java
@@ -1,9 +1,14 @@
 package Java;
 
+import java.util.HashMap;
+
 public class Vehicle {
     String brand;
     String registerNumber;
     int price;
+
+    // Lista med alla bilar
+    HashMap<String, Integer> car = new HashMap<>();
 }
 
 // Hashmap med olika bilar
